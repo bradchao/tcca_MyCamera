baa28602224f16af94aae3e8e01ed2324629c0f3
Brad Chao <brad@brad.tw>
2017-11-11 09:51:12 +0800

    201711110951

--- app/src/main/java/com/example/administrator/mycamera/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycamera/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,4 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void test1(View view) {
+
+    }
+    public void test2(View view) {
+    }
+    public void test3(View view) {
+    }
 }
