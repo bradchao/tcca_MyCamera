3c58af5ae2e680bd53f6bed38e4dfca04b1034ea
Brad Chao <brad@brad.tw>
2017-11-11 14:53:37 +0800

    201711111453

--- app/src/main/java/com/example/administrator/mycamera/PreviewActivity.java
+++ app/src/main/java/com/example/administrator/mycamera/PreviewActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
 public class PreviewActivity extends AppCompatActivity {
     private SurfaceView surfaceView;
@@ -19,7 +20,33 @@ public class PreviewActivity extends AppCompatActivity {
         surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
 
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceHolder.addCallback(new MyCallback());
 
+        surfaceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
+
+    private class MyCallback implements SurfaceHolder.Callback {
+
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+    }
+    
 }
